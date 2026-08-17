8bd72ee07c952fa9c941cdf43e27220056f6cdb5
ANUPAM PRADHAN <anupampradhan161@gmail.com>
2026-08-17 15:48:13 +0530

    favi icon changed and added adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -146,6 +146,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7702804360140517"
+          crossOrigin="anonymous"
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(softwareSchema) }}
         />
